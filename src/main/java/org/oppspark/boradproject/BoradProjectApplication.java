b4e0b756fa89200bc83223a5049bc6d1ccb92bc1
OppSpark <picks5@naver.com>
2024-03-02 15:03:49 +0900

    first commit

---
+++ src/main/java/org/oppspark/boradproject/BoradProjectApplication.java
@@ -0,0 +1,13 @@
+package org.oppspark.boradproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoradProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoradProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/oppspark/boradproject/BoradProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oppspark.boradproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoradProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
